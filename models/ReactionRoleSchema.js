3b975380174b312bb4475971fb32fd0b651c95d3
enderfire99 <nachoqmt@gmail.com>
2022-01-01 20:14:31 -0300

    Cambio para agregar channelD al schema

--- models/ReactionRoleSchema.js
+++ models/ReactionRoleSchema.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose')
 
 const reqString = 'type: String, required: true'
 
-const reactionRolesSchema = new mongoose.Schema({
+const reactionRoleSchema = new mongoose.Schema({
     guildID: {reqString}, //Para saber en que guild esta pasandio
+    channelID: {reqString},
     messageID: {reqString}, //para definir el mensaje si es que no creamos uno
     emojiID: {reqString}, //definimos el emoji
     action: {reqString}, //si lo quita y agrega, agrega o elimina
     roleID: {reqString} // que rol va a modificar
 })
 
-const model = mongoose.model("ReactionRolesModels", reactionRolesSchema)
+const model = mongoose.model("ReactionRoleModels", reactionRoleSchema)
 
 module.exports = model;

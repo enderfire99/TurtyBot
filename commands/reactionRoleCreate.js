a7abe3d3db36e667220ac5b046687e03ba276585
enderfire99 <nachoqmt@gmail.com>
2022-01-04 16:38:18 -0300

    Reaction role pedir canal y mensaje

--- commands/reactionRoleCreate.js
+++ commands/reactionRoleCreate.js
@@ -17,21 +17,54 @@ module.exports = {
             return m.author.id === message.author.id
         }
 
-        const collector = message.channel.createMessageCollector({
+        const collector_channel = message.channel.createMessageCollector({
             filter, time: 1000 * 20, max: 1
         })
 
-        collector.on('collect', m => console.log(`Collected ${m.content} \n`));
+        collector_channel.on('collect', m => console.log(`Collected ${m.content} \n`));
 
-        collector.on('end', collected => {
+        collector_channel.on('end', collected => {
+            if(collected.size == 0) message.channel.send("time out")
+
             collected.forEach(message => {
                 if (message.guild.channels.cache.has(message.content))
                 {
-                    message.channel.send('existe')
+                    id_channel = message.guild.channels.cache.get(message.content);
+
+                    message.channel.send('selecciono su canal, que mensaje es?')
+
+                    const collector_msg = message.channel.createMessageCollector({
+                        filter, time: 1000 * 20, max: 1
+                    })
+            
+                    collector_msg.on('collect', m => console.log(`Collected ${m.content} \n`));
+            
+                    collector_msg.on('end', collected => {
+                        if(collected.size == 0) message.channel.send("time out")
+
+                        msg_error = true;
+
+                        collected.forEach(async message => {
+                            await id_channel.messages.fetch(message.content).then(
+                                message => {
+                                    id_message = message.content;
+                                }
+                            ).catch(async () => {
+                                console.error;
+                                msg_error = false;
+                                await message.channel.send('no existe');
+                                
+                            });
+
+                            if (msg_error)
+                            {
+                                message.channel.send('selecciono su mensaje, que emoji necesita??')
+                            }
+                        });
+                    });
                 }
-                else
-                {
-                    message.channel.send('no existe')
+                else{
+                    message.channel.send("eso no es un canal");
                 }
             });
         });

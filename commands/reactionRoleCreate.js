9a68ec1159e57f6357f6f95ffa64c1723a7b7a46
enderfire99 <nachoqmt@gmail.com>
2022-01-07 00:25:05 -0300

    (no branch): Auto stash before revert of "Merge pull request #3 from enderfire99/reactionroles"

--- commands/reactionRoleCreate.js
+++ commands/reactionRoleCreate.js
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 const reactionRoleSchema = require("../models/reactionRoleSchema");
+=======
+require('dotenv').config();
+
+const reactionRoleModel = require("../models/reactionRoleSchema");
+>>>>>>> Stashed changes
 
 
 
@@ -27,7 +33,168 @@ module.exports = {
             collected.forEach(message => {
                 if (message.guild.channels.cache.has(message.content))
                 {
+<<<<<<< Updated upstream
                     message.channel.send('existe')
+=======
+                    id_channel = message.guild.channels.cache.get(message.content);
+
+                    message.channel.send('selecciono su canal, que mensaje es?')
+
+                    const collector_msg = message.channel.createMessageCollector({
+                        filter, time: 1000 * 20, max: 1
+                    })
+            
+                    collector_msg.on('collect', m => console.log(`Collected ${m.content} \n`));
+            
+                    collector_msg.on('end', collected => {
+                        if(collected.size == 0) message.channel.send("time out");
+                        msg_error = true;
+
+                        collected.forEach(async message => {
+                            await id_channel.messages.fetch(message.content).then(
+                                message => {
+                                    id_message = message.content;
+                                }
+                            ).catch(async () => {
+                                console.error;
+                                msg_error = false;
+                                await message.channel.send('no existe');
+                            });
+
+                            id_message = message.content;
+                            
+                            if (msg_error)
+                            {
+                                message.channel.send('selecciono su mensaje, reaccionar a su anterior mensaje con el emoji que va a usar')
+
+                                const emoji_collector = message.createReactionCollector({ emoji_filter, time: 15000, max: 1});
+
+                               emoji_error  = true;
+
+                                emoji_collector.on('collect', (reaction, user) => {
+                                    console.log(`emoji Collected ${reaction.emoji.name} from ${user.tag}`);
+                                });
+
+                                emoji_collector.on('end', async collected => {
+                                    if (collected.size == 0) message.channel.send("time out")
+
+                                    collected.forEach(async collected => {
+                                        await message.guild.emojis.fetch(collected.emoji.id).then(emoji => {
+                                            id_emoji = emoji.id;
+                                        }).catch(async () => {
+                                            console.error;
+                                            emoji_error = false;
+                                            await message.channel.send('no existe');
+                                        });
+
+                                        name_emoji = collected.emoji.name;
+                                    
+                                        if(emoji_error)
+                                        {
+                                            await message.react('🇦');
+                                            await message.react('🇧');
+                                            await message.react('🇨'); 
+
+                                            await message.channel.send('selecciono su emoji, accion A dar, accion B borrar, accion C ambas');
+
+                                            const action_filter = (reaction, user) => {
+                                                return (reaction.emoji.name == '🇦' || reaction.emoji.name == '🇧' || reaction.emoji.name == '🇨') && !user.bot && user.id === message.author.id;
+                                            }
+
+                                            const action_collector = message.createReactionCollector({ action_filter, time: 15000, max: 1});
+
+                                            action_collector.on('collect', (reaction, user) => {
+                                                console.log(`emoji for action Collected ${reaction.emoji.name} from ${user.tag}`);
+                                            });
+
+                                            action_collector.on('end', async collected => {
+                                                if (collected.size == 0) message.channel.send("time out")
+                                                
+                                                collected.forEach(async collected => {
+                                                    await message.guild.emojis.fetch(collected.emoji.id).then(emoji => {
+                                                        action_emoji = emoji.name;
+                                                    }).catch(async () => {
+                                                        console.error;
+                                                        action_error = false;
+                                                        await message.channel.send('no existe');
+                                                    });
+
+                                                    switch(collected.emoji.name)
+                                                    {
+                                                        case '🇦':
+                                                            message.channel.send("accion dar")
+                                                            id_action = 1;
+                                                            break;
+                                                        case '🇧':
+                                                            message.channel.send("accion borrar")
+                                                            id_action = 2;
+                                                            break;
+                                                        case '🇨':
+                                                            message.channel.send("accion dar/borrar")
+                                                            id_action = 3;
+                                                            break;
+                                                        default:
+                                                            console.log("error");
+                                                    }
+
+                                                    if(id_action === 1 || id_action === 2 || id_action === 3)
+                                                    {
+                                                        message.channel.send('envie el id del rol que quiere');
+
+                                                        const collector_channel = message.channel.createMessageCollector({ filter, time: 1000 * 20, max: 1 });
+
+                                                        collector_channel.on('collect', m => console.log(`Collected ${m.content} \n`));
+
+                                                        collector_channel.on('end', async collected => {
+                                                            if(collected.size == 0) message.channel.send("time out")
+
+                                                            rol_error = true;
+
+                                                            collected.forEach(async message => {
+                                                                message.guild.roles.fetch(message.content).then( role => {
+                                                                    id_rol = message.content;
+                                                                }).catch(() => {
+                                                                    console.error;
+                                                                    rol_error = false;
+                                                                    message.channel.send('no existe el rol');
+                                                                });
+                                                                
+                                                                id_rol = message.content;
+
+                                                                if(rol_error)
+                                                                {
+                                                                    message.channel.send(`guild: ${message.guild.id} | channel: ${id_channel} | message: ${id_message} | emoji: ${name_emoji} | action: ${id_action} | rol: ${id_rol}`)
+                                                                    console.log(`guild: ${message.guild.id} | channel: ${id_channel} | message: ${id_message} | emoji: ${name_emoji} | action: ${id_action} | rol: ${id_rol}`)
+                                                                
+                                                                    let reactionRole = await reactionRoleModel.create({
+                                                                        guildID: message.guild.id,
+                                                                        channelID: id_channel,
+                                                                        messageID: id_message,
+                                                                        emojiNAME: name_emoji,
+                                                                        action: id_action,
+                                                                        roleID: id_rol,
+                                                                    });
+
+                                                                    await reactionRole.save();
+
+                                                                    message.channel.send('se guardo en la base de datos')
+                                                                }
+                                                            });
+                                                        });
+                                                    }
+                                                    else
+                                                    {
+                                                        message.channel.send('no esta bien el emoji')
+                                                    }
+                                                });
+                                            });
+                                        } 
+                                    });
+                                });
+                            }
+                        });
+                    });
+>>>>>>> Stashed changes
                 }
                 else
                 {
--- models/ReactionRoleSchema.js
+++ models/ReactionRoleSchema.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose')
 
 const reqString = {
     type: String,
-    required: true
+    require: true
 }
 
 const reactionRoleSchema = new mongoose.Schema({
     guildID: reqString, //Para saber en que guild esta pasandio
     channelID: reqString, // para saber en que canal buscar
     messageID: reqString, //para definir el mensaje si es que no creamos uno
-    emojiID: reqString, //definimos el emoji
-    action: reqString, //si lo quita y agrega, agrega o elimina
+    emojiNAME: reqString, //definimos el emoji
+    action: {type: Number, require: true}, //si lo quita y agrega, agrega o elimina
     roleID: reqString // que rol va a modificar
 })
 
